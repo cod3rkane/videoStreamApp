911202d97aa5bb7c096a8ef8e0be6efc171b221b
Cod3rkane <jcesar.developer@gmail.com>
2017-01-02 23:23:31 -0200

    Added Socket functions

--- app/Components/VideoPlay.component.ts
+++ app/Components/VideoPlay.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   moduleId: module.id
 })
 export class VideoPlayComponent implements OnInit {
+
   ngOnInit() {
     let video = document.getElementById('videoPlay');
 
@@ -14,11 +15,31 @@ export class VideoPlayComponent implements OnInit {
       userMedia.then((stream) => {
         // Create the Blob URL
         video.setAttribute('src', window.URL.createObjectURL(stream));
+        let ws = new WebSocket("ws://192.168.0.10:9002");
+        ws.onopen = e => this.onOpen(e, ws);
+        ws.onmessage = e => this.onMessage(e);
+        ws.onclose = e => this.onClose(e);
       }, (error) => {
         alert('Error: ' + error.message)
       });
     } else {
       alert ("Sorry, your devices don't have support for this application. ;(");
     }
+  }
+
+  private onOpen(evt: Event, ws: WebSocket) {
+    console.log(evt);
+    ws.send("Hello, javascript sent this message!");
+    setTimeout(() => {
+      ws.send("By Cod3r Kane");
+    }, 2000);
+  }
+
+  private onMessage(evt: Event) {
+    console.log(evt);
+  }
+
+  private onClose(evt: Event) {
+    console.log(evt);
   }
 }
